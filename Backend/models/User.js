eec21f63922d0523f2fbe85df8f2584607d0b4d8
YoussifZyada <youssifzyd1@gmail.com>
2026-04-08 12:18:22 +0200

    fix: ensure next() is called in pre-save hook for User model

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -13,12 +13,14 @@ const userSchema = new mongoose.Schema({
 userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
   return await bcrypt.compare(candidatePassword, userPassword);
 };
-userSchema.pre('save', async function () {
+userSchema.pre('save', async function (next) {
   const user = this;
 
   if (user.isModified('password')) {
     user.password = await bcrypt.hash(user.password, 10);
   }
+  next();
+
 });
 
 export default mongoose.model('User', userSchema);
